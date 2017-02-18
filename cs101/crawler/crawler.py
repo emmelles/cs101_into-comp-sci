f132616f6c0279f9807933e8d7d0ff90b6d119f7
MLS <mls54@st-andrews.ac.uk>
2017-02-18 17:20:19 +0000

    Edit crawler for iterating over whole page content to pull out all links

--- cs101/crawler/crawler.py
+++ cs101/crawler/crawler.py
@@ -7,7 +7,16 @@
 myPage =('<div id="top_bin"><div id="top_content" class="width960">'
 '<div class="udacity float-left"><a href="http://udacity.com">')
 
-myPage1=("potatoes")
+myPage1=('''Comics I enjoy:<a href="http://threewordphrase.com/">Three Word Phrase</a>,
+        <a href="http://www.smbc-comics.com/">SMBC</a>,
+        <a href="http://www.qwantz.com">Dinosaur Comics</a>,
+        <a href="http://oglaf.com/">Oglaf</a> (nsfw),
+        <a href="http://www.asofterworld.com">A Softer World</a>,
+        <a href="http://buttersafe.com/">Buttersafe</a>,
+        <a href="http://pbfcomics.com/">Perry Bible Fellowship</a>,
+        <a href="http://questionablecontent.net/">Questionable Content</a>,
+        <a href="http://www.buttercupfestival.com/">Buttercup Festival</a>,
+        <a href="http://www.mspaintadventures.com/?s=6&p=001901">Homestuck</a>,''')
 
 def get_next_target(page):
     ''' Returns the first url found and the appropriate
@@ -18,12 +27,17 @@ def get_next_target(page):
     start_url=page.find(url_head)
     if start_url == -1:
         return None,0
-    end_url=page.find(url_tail, start_url)
+    end_url=page.find(url_tail, start_url+len(url_head))
     url=page[start_url+len(url_head):end_url]
     return url, end_url
 
-link, chop=get_next_target(myPage)
-link1, chop1=get_next_target(myPage1)
+def print_all_links(page):
+    while True:
+        link, chop=get_next_target(page)
+        if link:
+            print link
+            page=page[chop:]
+        else:
+            break
 
-print link is None, chop
-print link1 is None, chop1
+print_all_links(myPage1)
