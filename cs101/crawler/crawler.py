8795992f74d7910fa8fbaa8ae20378b17de461c7
MLS <mls54@st-andrews.ac.uk>
2017-02-18 17:02:11 +0000

    Add function to crawler so pulling the url becomes easy to iterate

--- cs101/crawler/crawler.py
+++ cs101/crawler/crawler.py
@@ -4,15 +4,26 @@
 # start_link to be the value of the position
 # where the first '<a href=' occurs in a page.
 
-page =('<div id="top_bin"><div id="top_content" class="width960">'
+myPage =('<div id="top_bin"><div id="top_content" class="width960">'
 '<div class="udacity float-left"><a href="http://udacity.com">')
 
-url_head='<a href="'
-url_tail='"'
+myPage1=("potatoes")
 
-start_url=page.find(url_head)+len(url_head)
-end_url=page.find(url_tail, start_url)
+def get_next_target(page):
+    ''' Returns the first url found and the appropriate
+    location to truncate the string to iterate
+    and find the next one'''
+    url_head='<a href="'
+    url_tail='"'
+    start_url=page.find(url_head)
+    if start_url == -1:
+        return None,0
+    end_url=page.find(url_tail, start_url)
+    url=page[start_url+len(url_head):end_url]
+    return url, end_url
 
-url=page[start_url:end_url]
+link, chop=get_next_target(myPage)
+link1, chop1=get_next_target(myPage1)
 
-print url
+print link is None, chop
+print link1 is None, chop1
