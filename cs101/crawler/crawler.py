d51b74ecbee6c26017eeaa5efedf3b0e3c09387b
MLS <mls54@st-andrews.ac.uk>
2017-02-18 16:20:10 +0000

    Change definitions in crawler slightly to match course

--- cs101/crawler/crawler.py
+++ cs101/crawler/crawler.py
@@ -4,15 +4,15 @@
 # start_link to be the value of the position
 # where the first '<a href=' occurs in a page.
 
-page='''<div id="top_bin"> <div id="top_content" class="width960">
-   <div class="udacity float-left"> <a href="http://blag.xkcd.com/">'''
+page =('<div id="top_bin"><div id="top_content" class="width960">'
+'<div class="udacity float-left"><a href="http://udacity.com">')
 
-link_head='<a href="'
-link_tail='"'
+url_head='<a href="'
+url_tail='"'
 
-start_link=page.find(link_head)+len(link_head)
-end_link=page.find(link_tail, start_link)
+start_url=page.find(url_head)+len(url_head)
+end_url=page.find(url_tail, start_url)
 
-link=page[start_link:end_link]
+url=page[start_url:end_url]
 
-print link
+print url
