29a6b0d0979ba8c1442f4d70de7360ef4f55e0a1
MLS <mls54@st-andrews.ac.uk>
2017-02-22 10:50:10 +0000

    Add get_page to crawler to pull page content by url

--- cs101/crawler/crawler.py
+++ cs101/crawler/crawler.py
@@ -1,22 +1,13 @@
 #!/usr/bin/env python
 
-# Write code that initializes the variable
-# start_link to be the value of the position
-# where the first '<a href=' occurs in a page.
+# Slowly building a web crawler following course cs101 on Udacity.
 
-myPage =('<div id="top_bin"><div id="top_content" class="width960">'
-'<div class="udacity float-left"><a href="http://udacity.com">')
-
-myPage1=('''Comics I enjoy:<a href="http://threewordphrase.com/">Three Word Phrase</a>,
-        <a href="http://www.smbc-comics.com/">SMBC</a>,
-        <a href="http://www.qwantz.com">Dinosaur Comics</a>,
-        <a href="http://oglaf.com/">Oglaf</a> (nsfw),
-        <a href="http://www.asofterworld.com">A Softer World</a>,
-        <a href="http://buttersafe.com/">Buttersafe</a>,
-        <a href="http://pbfcomics.com/">Perry Bible Fellowship</a>,
-        <a href="http://questionablecontent.net/">Questionable Content</a>,
-        <a href="http://www.buttercupfestival.com/">Buttercup Festival</a>,
-        <a href="http://www.mspaintadventures.com/?s=6&p=001901">Homestuck</a>,''')
+def get_page(url):
+    try:
+        import urllib
+        return urllib.urlopen(url).read()
+    except:
+        return ''
 
 def get_next_target(page):
     ''' Returns the first url found and the appropriate
@@ -40,4 +31,4 @@ def print_all_links(page):
         else:
             break
 
-print_all_links(myPage1)
+print_all_links(get_page('http://www.udacity.com/cs101x/index.html'))
